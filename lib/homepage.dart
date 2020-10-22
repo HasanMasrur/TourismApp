71acd03402bf69512d39a57856d12b8d69e62730
Hasan <hasan@gmail.com>
2020-10-22 12:09:25 +0600

    added textfild,and wellcome

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _HomePage();
   }
 }
@@ -16,22 +16,70 @@ class _HomePage extends State<HomePage> {
     return Scaffold(
       key: _globalKey,
       drawer: Drawer(),
-      body: Column(children: [
-        Container(
-          margin: EdgeInsets.only(top: 50),
-          height: 50,
-          color: Colors.blue,
-          child: Row(
-            children: [
-              IconButton(
-                  icon: Icon(Icons.menu),
-                  onPressed: () {
-                    _globalKey.currentState.openDrawer();
-                  })
-            ],
-          ),
-        )
-      ]),
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 50, left: 15),
+              height: 50,
+              child: Row(
+                children: [
+                  IconButton(
+                      icon: FaIcon(FontAwesomeIcons.alignLeft),
+                      onPressed: () {
+                        _globalKey.currentState.openDrawer();
+                      }),
+                  SizedBox(
+                    width: 80,
+                  ),
+                  Container(
+                    width: 230,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        suffixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 15),
+              alignment: Alignment.topLeft,
+              child: Text(
+                ' Welcome ? \n Find Your Trip Here',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20),
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Popular',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              height: 180,
+              width: 250,
+              color: Colors.blue,
+            )
+          ]),
     );
   }
 }
