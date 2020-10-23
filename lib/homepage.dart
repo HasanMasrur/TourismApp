05706be78b5f4b6def29d2ac9f992d79fe6b3915
Hasan <hasan@gmail.com>
2020-10-24 01:09:53 +0600

    added popular place listview

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
+import 'package:TourismApp/popular_box.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,73 +17,88 @@ class _HomePage extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       key: _globalKey,
       drawer: Drawer(),
       body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 50, left: 15),
-              height: 50,
-              child: Row(
-                children: [
-                  IconButton(
-                      icon: FaIcon(FontAwesomeIcons.alignLeft),
-                      onPressed: () {
-                        _globalKey.currentState.openDrawer();
-                      }),
-                  SizedBox(
-                    width: 80,
-                  ),
-                  Container(
-                    width: 230,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        suffixIcon: Icon(
-                          Icons.search,
-                          color: Colors.black,
-                        ),
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 50, left: 15),
+            height: 50,
+            child: Row(
+              children: [
+                IconButton(
+                    icon: FaIcon(FontAwesomeIcons.alignLeft),
+                    onPressed: () {
+                      _globalKey.currentState.openDrawer();
+                    }),
+                SizedBox(
+                  width: 80,
+                ),
+                Container(
+                  width: 230,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      suffixIcon: Icon(
+                        Icons.search,
+                        color: Colors.black,
                       ),
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
-            Container(
-              margin: EdgeInsets.only(left: 15),
-              alignment: Alignment.topLeft,
-              child: Text(
-                ' Welcome ? \n Find Your Trip Here',
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold),
-              ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 15),
+            alignment: Alignment.topLeft,
+            child: Text(
+              ' Welcome ? \n Find Your Trip Here',
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
             ),
-            SizedBox(
-              height: 30,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 20),
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Popular',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
             ),
-            Container(
-              margin: EdgeInsets.only(left: 20),
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Popular',
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-              ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            height: 250,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                PopularBox(
+                  image: AssetImage('assets/darjeeling.jpg'),
+                  placename: "darjeeling",
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                PopularBox(
+                  image: AssetImage('assets/Sikkim.png'),
+                  placename: "Gangtok",
+                )
+              ],
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              height: 180,
-              width: 250,
-              color: Colors.blue,
-            )
-          ]),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/popular_box.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class PopularBox extends StatelessWidget {
+  String placename;
+  ImageProvider image;
+  PopularBox({
+    this.placename,
+    this.image,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 180,
+          child: Stack(
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.only(left: 20),
+                height: 160,
+                width: 280,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  image: DecorationImage(
+                    image: image,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              // Center(
+              //     child: Image.asset(
+              //   'assets/darjeeling.jpg',
+              //   height: size.height,
+              //   width: size.width,
+              //   fit: BoxFit.fill,
+              // )),
+              Positioned(
+                  left: 220,
+                  top: 140,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.red,
+                    radius: 20,
+                    child: Icon(
+                      Icons.add_circle,
+                      size: 30,
+                    ),
+                  )),
+            ],
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.only(left: 20),
+              child: Text(
+                'Darjeeling',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(left: 20),
+                child: RatingBar(
+                  initialRating: 03,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemSize: 20,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                  itemBuilder: (context, _) {
+                    return Icon(
+                      Icons.star,
+                      size: 30,
+                      color: Colors.amber,
+                    );
+                  },
+                  onRatingUpdate: (rating) {
+                    print(rating);
+                  },
+                ))
+          ],
+        )
+      ],
+    );
+  }
+}
